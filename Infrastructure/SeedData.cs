7738405452bb1b18ab7da0960e28da62c35feb88
Pavyelm <pavyel.m@gmail.com>
2022-10-14 12:16:22 +0200

    FullProject

---
+++ Infrastructure/SeedData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CalProTein.Models;
+
+namespace CalProTein.Infrastructure
+{
+    public class SeedData
+    {
+        public static void SeedDatabase(DataContext context)
+        {
+            context.Database.Migrate();
+
+            if (!context.Products.Any())
+            {
+                Category måltid = new Category { Name = "Måltid", Slug = "måltid" };
+                Category frukt = new Category { Name = "Frukt", Slug = "frukt" };
+
+                context.Products.AddRange(
+                        new Product
+                        {
+                            Name = "Gainomamx",
+                            Slug = "gainomamx",
+                            Description = "proteinshasxke",
+                            Cal = 170,
+                            Protein = 30,
+                            Category = måltid,
+                           
+                        }
+                    
+
+
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
